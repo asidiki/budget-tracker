f332f40113fe6151c4ca01c8a913ad529dc3b49f
Ansab Sidiki <ansab@sidiki.io>
2022-06-22 21:37:02 -0400

    final

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
+const cors = require('cors')
+
 
 router.post("/api/transaction", ({body}, res) => {
   Transaction.create(body)
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
@@ -15,6 +16,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+app.use(cors({
+  origin: ['*']
+}));
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
